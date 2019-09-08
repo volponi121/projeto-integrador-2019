9f8c0a804abf9571272f4d43c7958d6cad3c65d1
Fabio <fabioaugusto801@gmail.com>
2019-09-07 21:55:02 -0300

    Finalizando organização do projeto

--- backend/ProjetoIntegrador/src/main/java/br/com/projetointegrador2019/ProjetoIntegrador/ProjetoIntegradorApplication.java
+++ backend/ProjetoIntegrador/src/main/java/br/com/projetointegrador2019/ProjetoIntegrador/ProjetoIntegradorApplication.java
@@ -1,13 +1,13 @@
-package com.projetointegrador2019.demo;
+package br.com.projetointegrador2019.ProjetoIntegrador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ProjetoIntegradorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ProjetoIntegradorApplication.class, args);
 	}
 
 }
--- backend/ProjetoIntegrador/src/test/java/br/com/projetointegrador2019/ProjetoIntegrador/ProjetoIntegradorApplicationTests.java
+++ backend/ProjetoIntegrador/src/test/java/br/com/projetointegrador2019/ProjetoIntegrador/ProjetoIntegradorApplicationTests.java
@@ -1,4 +1,4 @@
-package com.projetointegrador2019.demo;
+package br.com.projetointegrador2019.ProjetoIntegrador;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class ProjetoIntegradorApplicationTests {
 
 	@Test
 	public void contextLoads() {
